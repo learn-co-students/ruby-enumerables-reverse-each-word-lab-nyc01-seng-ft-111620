4195ee4cc65c94172b8838b7799e2935401a513d
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-19 16:25:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ def reverse_each_word(string)
   #puts reversed_in_place
   return reversed_in_place.join(' ')
 end
+
+# def reverse_each_word(string)
+#   splitted = string.split(' ')
+#   reversed_in_place = []
+#   splitted.collect do |ele|
+#     reversed_in_place << ele.reverse
+#   end
+#   #puts reversed_in_place
+#   return reversed_in_place.join(' ')
+# end
